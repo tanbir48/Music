6257e9cd7c9e0f543bb1ecf8512fbbf658d4546e
Tanbir Hasan <mdtanbir576@gmail.com>
2021-12-23 22:26:04 +0600

    commit

--- js/script.js
+++ js/script.js
@@ -1,34 +1,19 @@
-// var button = document.getElementById("nav-button");
-// button.addEventListener("click", function () {
-//   var nav = document.getElementById("nav");
-//   var menu = document.getElementById("menu");
-//   menu.style.display = "block";
-//   nav.style.display = "flex";
-//   nav.style.flexDirection = "column";
-//   var crossbutton = document.getElementById("mobile-view-cross");
-//   crossbutton.style.display = "block";
-//   crossbutton.style.marginLeft = "10px";
-// });
-
-// var crossbutton = document.getElementById("nav-button-cross");
-// crossbutton.addEventListener("click", function () {
-//   var menu = document.getElementById("menu");
-//   menu.style.display = "none";
-//   crossbutton.style.display = " ";
-//   crossbutton.onclick = window.location.reload();
-// });
-
 var button = document.getElementById("nav-button");
 button.addEventListener("click", function () {
   var nav = document.getElementById("nav");
-
+  var menu = document.getElementById("menu");
+  menu.style.display = "block";
   nav.style.display = "flex";
   nav.style.flexDirection = "column";
+  var crossbutton = document.getElementById("mobile-view-cross");
+  crossbutton.style.display = "block";
+  crossbutton.style.marginLeft = "10px";
+});
 
+var crossbutton = document.getElementById("nav-button-cross");
+crossbutton.addEventListener("click", function () {
   var menu = document.getElementById("menu");
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
+  menu.style.display = "none";
+  crossbutton.style.display = " ";
+  crossbutton.onclick = window.location.reload();
 });
