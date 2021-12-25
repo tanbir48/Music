d6a6e9d6dc5d0e9c95cc58e9d905b59fb605c512
Tanbir Hasan <mdtanbir576@gmail.com>
2021-12-25 15:52:03 +0600

    commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
-var button = document.getElementById("nav-button");
-button.addEventListener("click", function () {
+// var button = document.getElementById("nav-button");
+// var menu = document.getElementById("menu");
+// button.addEventListener("click", function () {
+//   var nav = document.getElementById("nav");
+//   var menu = document.getElementById("menu");
+//   menu.style.display = "block";
+//   nav.style.display = "flex";
+//   nav.style.flexDirection = "column";
+//   var crossbutton = document.getElementById("mobile-view-cross");
+//   crossbutton.style.display = "block";
+//   crossbutton.style.marginLeft = "10px";
+// });
+
+// var crossbutton = document.getElementById("nav-button-cross");
+// crossbutton.addEventListener("click", function () {
+//   var menu = document.getElementById("menu");
+//   menu.style.display = "none";
+// });
+
+const hamburger = document.getElementById("nav-button");
+var navmenu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+  navmenu.classList.toggle("show");
   var nav = document.getElementById("nav");
-  var menu = document.getElementById("menu");
-  menu.style.display = "block";
   nav.style.display = "flex";
   nav.style.flexDirection = "column";
-  var crossbutton = document.getElementById("mobile-view-cross");
-  crossbutton.style.display = "block";
-  crossbutton.style.marginLeft = "10px";
-});
-
-var crossbutton = document.getElementById("nav-button-cross");
-crossbutton.addEventListener("click", function () {
-  var menu = document.getElementById("menu");
-  menu.style.display = "none";
-  crossbutton.style.display = " ";
-  crossbutton.onclick = window.location.reload();
 });
